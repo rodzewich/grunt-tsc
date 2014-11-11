c2872e69c7bceb69ca4d39655cb5cb405d96dbfe
rodzewich.oleg@gmail.com <RodZewCh1>
2014-11-11 16:13:21 +0300

    -

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,23 @@
 module.exports = function(grunt) {
     "use strict";
 
+    grunt.loadNpmTasks('grunt-git');
 
+    grunt.initConfig({
+        gitclone: {
+            typescript: {
+                options: {
+                    repository: 'https://github.com/Microsoft/TypeScript.git',
+                    branch: 'master',
+                    directory: 'temp/typescript'
+                }
+            },
+            xlib: {
+                repository: 'https://github.com/rodzewich/Class.git',
+                branch: 'master',
+                directory: 'temp/xlib'
+            }
+        }
+    });
 
 };
